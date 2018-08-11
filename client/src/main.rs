97f3ec714d42e7c940d8ea3c87da62d4578467b1
Trangar <Victor 'Trangar' Koenders>
2018-08-11 21:03:08 +0200

    Cleaned up client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,109 +1,20 @@
-#![allow(deprecated)]
-
 extern crate actix;
 extern crate actix_web;
 extern crate serde_json;
 extern crate shared;
-extern crate web_view;
 extern crate futures;
 
 mod web_server;
-// mod client;
 
-use shared::client::ClientHandle;
-use shared::ChannelUpdate;
-use std::thread::{sleep_ms, spawn};
-use web_view::*;
-
-#[derive(Default)]
-pub struct UIState {
-    handle: Option<ClientHandle>,
-}
-
-pub struct ClientState {
-    webview: MyUnique<WebView<'static, UIState>>,
-}
-
-#[derive(Debug)]
-pub struct Module {
-    id: String,
-    name: String,
-    ui: Option<String>,
-}
+#[cfg(platform = "linux")]
+const OPEN_COMMAND: &str = "xdg-open";
+#[cfg(platform = "windows")]
+const OPEN_COMMAND: &str = "start";
 
 fn main() {
     let url = web_server::serve();
-    let (_result_userdata, success) = run(
-        "Megabouncer",
-        Content::Url(url),
-        Some((800, 600)),
-        true,
-        true,
-        init_cb,
-        invoke_cb,
-        Default::default(),
-    );
-    println!("Success? {:?}", success);
-}
-
-fn init_cb(webview: MyUnique<WebView<'static, UIState>>) {
-    spawn(move || {
-        sleep_ms(1000);
-        let mut client = shared::client::Client::new(
-            "client",
-            ClientState {
-                webview: webview.clone(),
-            },
-        );
-        client.register_listener("*", client_all_received);
-        client.on_startup(|startup| {
-            startup
-                .emit
-                .push(serde_json::from_str("{\"action\":\"node.list\"}").unwrap());
-        });
-        let handle = client.launch_async();
-
-        webview.dispatch(move |_, state| {
-            let handle = handle.clone();
-            state.handle = Some(handle);
-        });
-    });
-}
-
-fn client_all_received(update: &mut ChannelUpdate<ClientState>) {
-    let str = format!(
-        "external_message_received(\"{}\", {:?})",
-        update.channel,
-        serde_json::to_string(&update.value).unwrap()
-    );
-    update.state.webview.dispatch(move |webview, _| {
-        webview.eval(&str);
-    });
-}
-
-fn invoke_cb(webview: &mut WebView<UIState>, arg: &str, userdata: &mut UIState) {
-    let mut iter = arg.split(':');
-    match iter.next() {
-        Some("exit") => {
-            webview.terminate();
-        }
-        Some("keydown") => {
-            if let Some(Ok(code)) = iter.next().map(|c| c.parse::<i32>()) {
-                if code == 27 {
-                    webview.terminate();
-                }
-            }
-        }
-        Some("log") => {
-            let line = iter.collect::<Vec<_>>().join(":");
-            println!("{}", line);
-        }
-        Some("emit") => {
-            let remaining = iter.collect::<Vec<_>>().join(":");
-            let value: serde_json::Value = serde_json::from_str(&remaining).unwrap();
-            println!("Sending {:?}", value);
-            userdata.handle.as_mut().unwrap().send(value);
-        }
-        _ => unimplemented!(),
+    std::process::Command::new(OPEN_COMMAND).arg(&url).spawn().unwrap();
+    loop {
+        std::thread::sleep(std::time::Duration::from_secs(5));
     }
 }
